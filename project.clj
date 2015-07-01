c7bf4d720809278f68d6064746a15efc62366ad3
Max Veytsman <maxim@ontoillogical.com>
2015-07-01 13:02:44 -0700

    Correctly grab crustacean for project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :eval-in-leiningen true
-  :dependencies [[crustacean "0.1.0-SNAPSHOT"]])
+  :dependencies [[appcanary/crustacean "0.1.1-SNAPSHOT"]])
